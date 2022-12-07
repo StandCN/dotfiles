6ce9104b7c29f94b3b1a6a3f39f14b53d2f0426f
StandCN <standyangcn@gmail.com>
2022-12-07 15:30:02 +0800

    Update install.sh

--- .init-cfg/install.sh
+++ .init-cfg/install.sh
@@ -23,6 +23,7 @@ config config --local status.showUntrackedFiles no
 echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
 echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.zshrc
 cd $HOME
-config checkout
+# checkout on focus
+config checkout -f
 
 
