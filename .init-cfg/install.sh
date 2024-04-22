d178e2f5d58e88de8e4a1e81eafaf1b830ece499
StandCN <16977189+StandCN@users.noreply.github.com>
2024-04-22 15:43:23 +0800

    Update install.sh

--- .init-cfg/install.sh
+++ .init-cfg/install.sh
@@ -1,10 +1,15 @@
 # init dir $HOME/.cfg
 mkdir -p $HOME/.cfg
 
+git config --global init.defaultBranch main
+
 git clone --bare git@github.com:StandCN/dotfiles.git  $HOME/.cfg
+
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+
 config config --local status.showUntrackedFiles no
 echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bashrc
+
 cd $HOME
 # checkout on focus
 config checkout -f
