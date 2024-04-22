22a55e2c5b6449b46d0093a7fddb44ec40097fa1
StandCN <16977189+StandCN@users.noreply.github.com>
2024-04-22 15:52:35 +0800

    Update install.sh
    modify ssh to https

--- .init-cfg/install.sh
+++ .init-cfg/install.sh
@@ -3,7 +3,7 @@ mkdir -p $HOME/.cfg
 
 git config --global init.defaultBranch main
 
-git clone --bare git@github.com:StandCN/dotfiles.git  $HOME/.cfg
+git clone --bare https://github.com/StandCN/dotfiles.git  $HOME/.cfg
 
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
